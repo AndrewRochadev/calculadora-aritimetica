0cacab9c13e312799d2b325b5576e5eeb7a61235
Andrew <andrew.pablo66@gmail.com>
2024-06-24 18:41:27 -0300

    primeira versão

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="mt-5">
+        <h1>Operações Aritimeticas</h1>
+        <p>Digite os valores para nome da tarefa</p>
+    </header>
+</template>
+
+<style scoped>
+    h1, p {
+        text-align: center;
+    }
+</style>
