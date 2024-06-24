0e239d0e96f3531e75e0b45b9794a83cc87b6f77
Andrew <andrew.pablo66@gmail.com>
2024-06-24 19:03:18 -0300

    alteração de titulo e parágrafo

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="mt-5">
-        <h1>Operações Aritimeticas</h1>
-        <p>Digite os valores para nome da tarefa</p>
+        <h1>Operações Aritmética</h1>
+        <p>Digite os valores para realizar o calculo</p>
     </header>
 </template>
 
